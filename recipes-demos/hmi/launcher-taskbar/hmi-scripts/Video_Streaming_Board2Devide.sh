73b189f0c3159d23ce02e55a5e9334a367296a23
Huynh Thanh Hung <hung.huynh.wz@renesas.com>
2019-03-19 18:13:12 +0700

    hmi: launcher-taskbar: Stream Board2Device: Replace broadcast IP and multicast address binary
    127.0.0.1 is used for broadcast network, which created mountpoint by
    test-multicast2
    
    Signed-off-by: Huynh Thanh Hung <hung.huynh.wz@renesas.com>

--- recipes-demos/hmi/launcher-taskbar/hmi-scripts/Video_Streaming_Board2Devide.sh
+++ recipes-demos/hmi/launcher-taskbar/hmi-scripts/Video_Streaming_Board2Devide.sh
@@ -13,14 +13,14 @@ printf "\n\e[31mNOTE:\n\
 \t* Click on Exit icon to exit from running applications\e[0m\n\n" > ${TTY}
 
 /home/root/IPShow/IPShow &
-/home/root/RZ_scripts/test-multicast2 &
+test-multicast2 &
 
 gst-launch-1.0 -e filesrc location=/home/root/videos/h264-hd-30.mp4 ! \
 qtdemux ! h264parse ! omxh264dec ! vspfilter ! \
 video/x-raw,format=NV12,width=640,height=480 ! omxh264enc \
 target-bitrate=5242880 ! h264parse ! video/x-h264,\
 stream-format=avc,alignment=au ! rtph264pay pt=96 name=pay0 \
-config-interval=3 ! udpsink host=127.255.255.255 port=5000
+config-interval=3 ! udpsink host=127.0.0.1 port=5000
 
 killall -9 -q test-multicast2
 killall -9 -q IPShow
