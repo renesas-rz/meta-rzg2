73524bfd31a0376304a2681a01a905247bd72fd9
Huynh Thanh Hung <hung.huynh.wz@renesas.com>
2019-04-05 08:42:25 +0700

    hmi: launcher-taskbar: add components to exit script
    Add some new components to exit.sh script
    
    Signed-off-by: Huynh Thanh Hung <hung.huynh.wz@renesas.com>

--- recipes-demos/hmi/launcher-taskbar/hmi-scripts/Exit.sh
+++ recipes-demos/hmi/launcher-taskbar/hmi-scripts/Exit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+killall -9 -q RZ_GST-demo_camera-preview.sh
 killall -9 -q RZ_GST-demo_encoded-review.sh
+killall -9 -q RZ_GST-demo_video-playback.sh
+killall -9 -q RZ_GST-network-demo_receive-video.sh
 killall -9 -q RZ_GST-network-demo_transmit-video.sh
 killall -9 -q RZ_GST-network-demo_video-wireless-streaming.sh
 
@@ -9,5 +12,6 @@ killall -9 -q sleep
 killall -2 -q gst-launch-1.0
 killall -9 -q Demo3D
 killall -q -9 test-multicast2
+killall -q -9 OGLES3ColourGrading
 
 sync
