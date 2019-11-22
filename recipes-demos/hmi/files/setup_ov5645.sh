f81d42e9a32f3654ea70fb7087aa6e525c53f1da
Ky Nguyen <ky.nguyen.uj@renesas.com>
2019-11-22 14:20:24 +0700

    recipes-demos: hmi: Fix the issue when executing demo apps
    - Fix issue: "./setup_ov5645.sh : No such file or directory" : change to use
    full-path /home/root/setup_ov5645.sh
    - Fix camera color format in "setup_ov5645.sh": color format UYVY_2X8 is not
    existed, it must be UYVY8_2X8.
    
    Signed-off-by: Ky Nguyen <ky.nguyen.uj@renesas.com>

--- recipes-demos/hmi/files/setup_ov5645.sh
+++ recipes-demos/hmi/files/setup_ov5645.sh
@@ -4,14 +4,14 @@ if [ "$(hostname)" == "hihope-rzg2n" ] || [ "$(hostname)" == "hihope-rzg2m" ]; t
 	export CAM_DEV="/dev/video5"
 	media-ctl -d /dev/media0 -r
 	media-ctl -d /dev/media0 -l "'rcar_csi2 fea80000.csi2':1 -> 'VIN5 output':0 [1]"
-	media-ctl -d /dev/media0 -V "'rcar_csi2 fea80000.csi2':1 [fmt:UYVY_2X8/1280x960 field:none]"
-	media-ctl -d /dev/media0 -V "'ov5645 2-003c':0 [fmt:UYVY_2X8/1280x960 field:none]"
+	media-ctl -d /dev/media0 -V "'rcar_csi2 fea80000.csi2':1 [fmt:UYVY8_2X8/1280x960 field:none]"
+	media-ctl -d /dev/media0 -V "'ov5645 2-003c':0 [fmt:UYVY8_2X8/1280x960 field:none]"
 elif [ "$(hostname)" == "ek874" ]; then
 	export CAM_DEVICE="/dev/video0"
 	media-ctl -d /dev/media0 -r
 	media-ctl -d /dev/media0 -l "'rcar_csi2 feaa0000.csi2':1 -> 'VIN5 output':0 [1]"
-	media-ctl -d /dev/media0 -V "'rcar_csi2 feaa0000.csi2':1 [fmt:UYVY_2X8/1280x960 field:none]"
-	media-ctl -d /dev/media0 -V "'ov5645 3-003c':0 [fmt:UYVY_2X8/1280x960 field:none]"
+	media-ctl -d /dev/media0 -V "'rcar_csi2 feaa0000.csi2':1 [fmt:UYVY8_2X8/1280x960 field:none]"
+	media-ctl -d /dev/media0 -V "'ov5645 3-003c':0 [fmt:UYVY8_2X8/1280x960 field:none]"
 else
 	echo "Unsupported platform: $(hostname)"
 fi
