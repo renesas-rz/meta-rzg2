1685dcb299219e9126e6cbc1bee4723bd897cafc
Hao Bui <hao.bui.yg@renesas.com>
2020-03-13 14:09:37 +0700

    multimedia: audio-init: Update audio init script to set default volume
    CIP19 kernel supports two audio codecs (HDMI audio and PCM5102a) but
    current audio init script only set default volume 20% for mixer control
    "DVC Out" 0. This may result in having to adjust DVC Out manually or
    no output sound when using "DVC Out" 1.
    
    Signed-off-by: Hao Bui <hao.bui.yg@renesas.com>

--- recipes-multimedia/alsa/audio-init/audio.sh
+++ recipes-multimedia/alsa/audio-init/audio.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-amixer set 'DVC In' 20%
-amixer set 'DVC Out' 20%
+hostname=$(cat /etc/hostname 2>/dev/null)
+
+case "$hostname" in
+  ek874)
+    amixer set 'DVC In',0 20%
+    amixer set 'DVC Out',0 20%
+    amixer set 'DVC In',1 20%
+    amixer set 'DVC Out',1 20%
+    ;;
+  hihope-rzg2m | hihope-rzg2n)
+    amixer set 'DVC In',1 20%
+    amixer set 'DVC Out',1 20%
+    ;;
+  *)
+    exit 1
+esac
+
+exit 0
